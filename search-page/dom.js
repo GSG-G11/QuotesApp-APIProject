cceb458b683a8a6e7ff60ec3d3f833a16e39d7a0
Fady-Alwazir <fady.alwazir@gmail.com>
2022-02-09 13:03:02 +0200

    card function and its style Realtes #10

---
+++ search-page/dom.js
@@ -0,0 +1,33 @@
+const createQuoteContent = (element, quote, auth, urlImg) => {
+  const container = document.querySelector(element);
+  const card = document.createElement("div");
+  card.setAttribute("class", "card");
+  container.appendChild(card);
+
+  const imgAndContentContainer = document.createElement("p");
+  imgAndContentContainer.setAttribute("class", "img-and-content");
+  card.appendChild(imgAndContentContainer);
+
+  const imgContainer = document.createElement("div");
+  imgContainer.setAttribute("class", "img-container");
+  imgAndContentContainer.appendChild(imgContainer);
+
+  const img = document.createElement("img");
+  imgContainer.appendChild(img);
+  img.src = `${urlImg}`;
+  img.alt = "author";
+
+  const quoteContent = document.createElement("p");
+  imgAndContentContainer.appendChild(quoteContent);
+  quoteContent.setAttribute("class", "quote-content");
+  quoteContent.textContent = `“${quote}”`;
+
+  const authorContainer = document.createElement("div");
+  card.appendChild(authorContainer);
+  authorContainer.setAttribute("class", "author-container");
+
+  const authorName = document.createElement("span");
+  authorName.setAttribute("class", "author-name");
+  authorContainer.appendChild(authorName);
+  authorName.textContent = `-${auth}`;
+};
